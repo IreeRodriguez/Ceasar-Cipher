e055145cda98579e10720b7b93b256f7d61a423b
IreeRodriguez <ire208@gmail.com>
2017-10-04 10:37:14 -0300

    comentarios

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var str = function(){
+var str = function(){ //funcion para validacion, retorna un String valido para cifrar o descifrar
     var num = /\d/;
-    var user = '';
+    var user = ''; // el String valido se guarda en esta variable durante la funcion.
     do {
         user = prompt('Escribe tu texto aqui.');
-        if (user===''){
+        if (user===''){ //valida cadena vacia
             alert('Debes escribir algo, no lo dejes en blanco.');
         }
-        else if (num.test(user)) {
+        else if (num.test(user)) { //hace un test para encontrar caracteres numericos dentro del string
             alert('No puedes usar numeros.');
         }
     } while (user===''|| num.test(user));
@@ -15,23 +15,23 @@ var str = function(){
 };
 
 
-var cipher = function(str){
+var cipher = function(str){ // funcion para el cifrado
     var output='';
-    for (var i=0;i<str.length;i++){
+    for (var i=0;i<str.length;i++){ //itera por cada caracter del string.
         var character = str[i];
-        var code = str.charCodeAt(i);
-        if (code>=97 && code <=122) {
+        var code = str.charCodeAt(i); //lo pasa a codigo ascii
+        if (code>=97 && code <=122) { //para letras minusculas
         character = String.fromCharCode(((code-97+33)%26)+97);
-        } else if (code>=65 && code<=90){
+    }   else if (code>=65 && code<=90){ //para letras mayusculas
             character = String.fromCharCode(((code-65+33)%26)+65);
         }
-        output += character;
+        output += character; //nuevo string cifrado
     }
 
     return output;
 };
 
-var decipher = function(str){
+var decipher = function(str){ // funcion descifrado, funciona igual que la de cifrado pero con la formula inversa
     var output='';
     for (var i=0;i<str.length;i++){
         var character = str[i];
@@ -41,24 +41,27 @@ var decipher = function(str){
         } else if (code>=65 && code<=90){
             character = String.fromCharCode(((code-65+19)%26)+65);
         }
-        output += character;
+        output += character; //nuevo string descrifrado
     }
 
     return output;
 };
 
-function valcipher (){
+function valcipher (){ //funcion incrustada en el boton en html que llama a la funcion para validar y a la de cifrado
     var output = document.getElementById('output');
 
     output.innerHTML = cipher(str());
 }
 
-function valdecipher (){
+function valdecipher (){ //funcion incrustada en el boton en html que llama a la funcion para validar y a la de descifrado
     var output = document.getElementById('output');
 
     output.innerHTML = decipher(str());
 }
 
+
+
+//Intento anterior para elegir una opcion
 // var path = confirm("cifrar o descifar?");
 //
 // if (path === true) {
